fc85c6955d30092ad63c8cd43fa02e43c9a93f69
Djonatan <djonatan.arns@projuris.com.br>
2022-10-21 15:28:44 -0300

    Teste de primeiro commit

--- src/main/java/br/com/arns/api/CursoUdemyRestApplication.java
+++ src/main/java/br/com/arns/api/CursoUdemyRestApplication.java
@@ -8,6 +8,7 @@ public class CursoUdemyRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CursoUdemyRestApplication.class, args);
+        System.out.println("Testando primeiro commit");
     }
 
 }
