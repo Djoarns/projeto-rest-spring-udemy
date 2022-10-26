76992fae173294016665a8b4b9551930b29b34c4
Djonatan <djonatan.arns@projuris.com.br>
2022-10-26 13:52:44 -0300

    [master] - adicionando profiles e fazendo testes para eles

--- src/main/java/br/com/arns/api/CursoUdemyRestApplication.java
+++ src/main/java/br/com/arns/api/CursoUdemyRestApplication.java
@@ -1,14 +1,26 @@
 package br.com.arns.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CursoUdemyRestApplication {
+
+    @Value("${paginacao.qtd_por_pagina}")
+    private int qtdPorPagina;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoUdemyRestApplication.class, args);
         System.out.println("Testando primeiro commit");
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> System.out.println("### Quantidade de elementos por página = " + qtdPorPagina);
     }
 
 }
--- src/test/java/br/com/arns/api/CursoUdemyRestApplicationTests.java
+++ src/test/java/br/com/arns/api/CursoUdemyRestApplicationTests.java
@@ -1,13 +1,24 @@
 package br.com.arns.api;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CursoUdemyRestApplicationTests {
+
+    @Value("${paginacao.qtd_por_pagina}")
+    private int qtdPorPagina;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void testCarregarContextoDeTeste() {
+        Assertions.assertEquals(100, qtdPorPagina);
+    }
 }
